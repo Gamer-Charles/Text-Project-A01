cf822cf72b87253a9d8317a234f3507f0c9126eb
Gamer-Charles <chaeltz07@gmail.com>
2026-01-31 19:54:50 -0600

    Add files via upload

---
+++ .github/workflows/team website/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log('Script is running!');
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark mode";
+    } else {
+        this.textContent = "Light mode";
+    }
+
+    console.log('current class name: ' + className);
+
+});
